8b08fbd76b198344825f9191e4df8b93d4c56d4f
heaven-wanderer <luckystqx@gmail.com>
2023-05-22 21:55:54 +0300

    Добавил класс создания доски

---
+++ Wolves and sheep/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wolves_and_sheep.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
