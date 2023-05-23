32b23a61f2e3ca8989ca91472e88e661d24f1f0c
heaven-wanderer <luckystqx@gmail.com>
2023-05-23 19:59:31 +0300

    Добавил содержимое в классы(которые не залились на гит)

--- Wolves and sheep/Models/Board.cs
+++ Wolves and sheep/Models/Board.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Wolves_and_sheep.Models
 {
-    internal class Board
+    public class Board : IEnumerable<Cell>
     {
+        private readonly Cell[,] area;
+        public CellValueEnum this[int row, int column]
+        {
+            get => area[row, column].Cellvalueenum;
+            set => area[row, column].Cellvalueenum = value;
+        }
+
+        public Board()
+        {
+            area = new Cell[8,8];
+            for (int i = 0; i < area.GetLength(0); i++)
+                for (int j = 0; j < area.GetLength(1); j++)
+                    area[i, j] = new Cell();
+        }
+
+        public IEnumerator<Cell> GetEnumerator()
+            => area.Cast<Cell>().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => area.GetEnumerator();
     }
 }
